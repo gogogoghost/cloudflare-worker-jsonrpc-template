3b8084111e98f545fb365b10b4d849565556c904
gogogoghost <ohleach@gmail.com>
2022-12-10 20:07:46 +0800

    add exception function

--- src/views.js
+++ src/views.js
@@ -1,6 +1,12 @@
-import { JSONRPCServer } from "json-rpc-2.0";
+import { JSONRPCServer,JSONRPCErrorException } from "json-rpc-2.0";
 
 const s=new JSONRPCServer()
+
+s.addMethod("main.fail",()=>{
+    throw new JSONRPCErrorException("I am custom error",100,{
+        detail:"I am detail"
+    })
+})
 
 s.addMethod("main.login",({user,pass})=>{
     return `${user} has logined with pass: ${pass}`
